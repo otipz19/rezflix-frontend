0f17f0c689addb02b85e09f9e1345535545ce11b
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-11-28 01:19:35 +0200

    Add form-input component

---
+++ src/app/shared/forms/components/form-input/form-input.component.ts
@@ -0,0 +1,43 @@
+import {ChangeDetectionStrategy, Component, computed, input} from '@angular/core';
+import {ErrorMessagePipe} from '@shared/forms/pipes/error-message.pipe';
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {
+  ZardFormControlComponent, ZardFormFieldComponent,
+  ZardFormLabelComponent,
+  ZardFormMessageComponent
+} from '@shared/zardui/components/form/form.component';
+import {ZardInputDirective} from '@shared/zardui/components/input/input.directive';
+
+export type FormInputFieldType =
+  | "email"
+  | "number"
+  | "tel"
+  | "text"
+  | "password";
+
+@Component({
+  selector: 'app-form-input',
+  imports: [
+    ErrorMessagePipe,
+    FormsModule,
+    ZardFormControlComponent,
+    ZardFormLabelComponent,
+    ZardFormMessageComponent,
+    ZardInputDirective,
+    ReactiveFormsModule,
+    ZardFormFieldComponent
+  ],
+  templateUrl: './form-input.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'block',
+  }
+})
+export class FormInputComponent {
+  readonly control = input.required<FormControl>();
+  readonly label = input.required<string>();
+  readonly type = input<FormInputFieldType>('text');
+  readonly placeholder = input<string>('');
+
+  protected readonly isRequired = computed(() => this.control().hasValidator(Validators.required));
+}
