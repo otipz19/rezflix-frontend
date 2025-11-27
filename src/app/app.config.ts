b71e1e49663420b7bdda137e74fc5d8d9147d1e8
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-11-27 20:16:50 +0200

    Add session restoration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {authInterceptor} from './auth/interceptors/auth.interceptor';
+import {restoreSessionProvider} from './auth/providers/restore-session.provider';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor])),
+    restoreSessionProvider
   ]
 };
