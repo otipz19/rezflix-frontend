46bdbb8419cb5f3d132d572b2dde2701f98e4d2d
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-11-27 17:32:40 +0200

    Add profile dropdown

--- src/app/pages/main.layout.ts
+++ src/app/pages/main.layout.ts
@@ -1,12 +1,20 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {ZardDropdownModule} from '@shared/zardui/components/dropdown/dropdown.module';
+import {ZardDividerComponent} from '@shared/zardui/components/divider/divider.component';
+import {ZardButtonComponent} from '@shared/zardui/components/button/button.component';
+import {ZardIconComponent} from '@shared/zardui/components/icon/icon.component';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main.layout.html',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    ZardDropdownModule,
+    ZardDividerComponent,
+    ZardButtonComponent,
+    ZardIconComponent
   ]
 })
 export class MainLayout {
