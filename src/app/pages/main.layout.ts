4a7989a6a34f7d7dc535f472adc37ca777576b9b
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-11-28 16:11:20 +0200

    Add link to movie upload page to header

--- src/app/pages/main.layout.ts
+++ src/app/pages/main.layout.ts
@@ -2,6 +2,9 @@ import {Component, inject} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {AuthService} from '../auth/services/auth.service';
 import {HeaderUserMenuComponent} from './components/header-user-menu/header-user-menu.component';
+import {ZardButtonComponent} from '@shared/zardui/components/button/button.component';
+import {UserRoleDto} from '../api';
+import {ZardIconComponent} from '@shared/zardui/components/icon/icon.component';
 
 @Component({
   selector: 'app-main-layout',
@@ -10,9 +13,12 @@ import {HeaderUserMenuComponent} from './components/header-user-menu/header-user
   imports: [
     RouterOutlet,
     HeaderUserMenuComponent,
-    RouterLink
+    RouterLink,
+    ZardButtonComponent,
+    ZardIconComponent
   ]
 })
 export class MainLayout {
   protected readonly auth = inject(AuthService);
+  protected readonly UserRoleDto = UserRoleDto;
 }
