92c3aa7971eaeac949ec501ede6c0ac04c544cfc
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-11-28 16:43:43 +0200

    Add create film page

---
+++ src/app/pages/films/create/page/create-film.page.ts
@@ -0,0 +1,55 @@
+import {ChangeDetectionStrategy, Component, inject, signal} from '@angular/core';
+import {FormInputComponent} from '@shared/forms/components/form-input/form-input.component';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {ZardButtonComponent} from '@shared/zardui/components/button/button.component';
+import {ZardCardComponent} from '@shared/zardui/components/card/card.component';
+import {Router} from '@angular/router';
+import {NotifyService} from '../../../../notify/services/notify.service';
+import {ControlsOf} from '@shared/forms/utils/controls-of';
+import {FilmControllerService, UpsertFilmDto} from '../../../../api';
+import {checkValidFormSubmit$} from '@shared/forms/utils/check-valid-form-submit';
+import {finalize, switchMap, tap} from 'rxjs';
+
+@Component({
+  selector: 'app-create-film',
+  templateUrl: './create-film.page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    FormInputComponent,
+    FormsModule,
+    ZardButtonComponent,
+    ZardCardComponent,
+    ReactiveFormsModule,
+  ],
+})
+export class CreateFilmPage {
+  private readonly fb = inject(FormBuilder).nonNullable;
+  private readonly router = inject(Router);
+  private readonly notify = inject(NotifyService);
+  private readonly api = inject(FilmControllerService);
+
+  protected readonly requestInProgress = signal(false);
+
+  protected readonly form = this.fb.group<ControlsOf<UpsertFilmDto>>({
+    // TODO: check validators on backend
+    title: this.fb.control('', [Validators.required]),
+    description: this.fb.control('', [Validators.required]),
+  });
+
+  protected onSubmit() {
+    checkValidFormSubmit$(this.form)
+      .pipe(
+        tap(() => this.requestInProgress.set(true)),
+        switchMap(() => {
+          const dto = this.form.getRawValue();
+          return this.api.createFilm(dto);
+        }),
+        finalize(() => this.requestInProgress.set(false)),
+        this.notify.notifyHttpRequest('Successfully created new movie!')
+      )
+      .subscribe(() => {
+        // TODO: change navigation to the created film editing page
+        this.router.navigate(['/']);
+      });
+  }
+}
--- src/app/pages/films/routes.ts
+++ src/app/pages/films/routes.ts
@@ -1,9 +1,16 @@
 import {Routes} from '@angular/router';
 import {FilmsPage} from './page/films.page';
+import {hasRoleRouteGuard} from '../../auth/route-guards/has-role-route.guard';
+import {UserRoleDto} from '../../api';
 
 export const FILMS_ROUTES: Routes = [
   {
     path: '',
     component: FilmsPage
+  },
+  {
+    path: 'create',
+    canActivate: [hasRoleRouteGuard(UserRoleDto.CONTENT_MANAGER)],
+    loadComponent: () => import('./create/page/create-film.page').then(m => m.CreateFilmPage)
   }
 ]
