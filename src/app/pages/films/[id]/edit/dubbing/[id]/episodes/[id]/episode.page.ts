b77a3bd9373749975d6eb580f3afa83e39a4afce
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-12-02 01:27:24 +0200

    Extract video player component

--- src/app/pages/films/[id]/edit/dubbing/[id]/episodes/[id]/episode.page.ts
+++ src/app/pages/films/[id]/edit/dubbing/[id]/episodes/[id]/episode.page.ts
@@ -1,41 +1,18 @@
-import {ChangeDetectionStrategy, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {getFromRoute} from '@shared/routing/get-from-route';
 import {EpisodeDto} from '../../../../../../../../api';
 import {RESOLVE_EPISODE_KEY} from './episode.resolver';
-import Player from 'video.js/dist/types/player';
-import videojs from 'video.js';
+import {VideoPlayerComponent} from './components/video-player/video-player.component';
 
 @Component({
   selector: 'app-episode-page',
   templateUrl: 'episode.page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [
+    VideoPlayerComponent
+  ],
 })
-export class EpisodePage implements OnInit, OnDestroy {
+export class EpisodePage {
   protected readonly episode = getFromRoute<EpisodeDto>(RESOLVE_EPISODE_KEY);
-
-  @ViewChild('target', {static: true}) target!: ElementRef;
-
-  private readonly options = {
-    fluid: false,
-    aspectRatio: '16:9',
-    autoplay: false,
-    sources: [{src: 'http://localhost:8080' + this.episode.hlsLink, type: 'application/vnd.apple.mpegurl'}]
-  };
-
-  private player?: Player;
-
-  // Instantiate a Video.js player OnInit
-  ngOnInit() {
-    this.player = videojs(this.target.nativeElement, this.options, function onPlayerReady() {
-      console.log('onPlayerReady', this);
-    });
-  }
-
-  // Dispose the player OnDestroy
-  ngOnDestroy() {
-    if (this.player) {
-      this.player.dispose();
-    }
-  }
+  protected readonly episodeLink = 'http://localhost:8080' + this.episode.hlsLink;
 }
