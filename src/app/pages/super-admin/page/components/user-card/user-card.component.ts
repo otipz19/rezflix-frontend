82aa800776985383611634102b5ce35916836d80
Mykola Prybysh <m.prybysh@ukma.edu.ua>
2025-12-02 19:26:24 +0200

    Add delete user button

--- src/app/pages/super-admin/page/components/user-card/user-card.component.ts
+++ src/app/pages/super-admin/page/components/user-card/user-card.component.ts
@@ -1,15 +1,26 @@
-import {ChangeDetectionStrategy, Component, input} from '@angular/core';
-import {UserDto, UserTypeDto} from '../../../../../api';
+import {ChangeDetectionStrategy, Component, input, inject} from '@angular/core';
+import {UserDto, UserTypeDto, UserControllerService} from '../../../../../api';
+import {DialogService} from '../../../../../core/dialog/services/dialog.service';
+import {NotifyService} from '../../../../../core/notify/services/notify.service';
+import {injectDispatch} from '@ngrx/signals/events';
+import {superAdminPageEvents} from '../../state/events';
+import {ZardButtonComponent} from '@shared/zardui/components/button/button.component';
+import {ZardIconComponent} from '@shared/zardui/components/icon/icon.component';
 
 @Component({
   selector: 'app-user-card',
-  imports: [],
+  imports: [ZardButtonComponent, ZardIconComponent],
   templateUrl: './user-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserCardComponent {
   readonly user = input.required<UserDto>();
   protected readonly UserTypeDto = UserTypeDto;
+
+  private readonly dialogService = inject(DialogService);
+  private readonly api = inject(UserControllerService);
+  private readonly notify = inject(NotifyService);
+  private readonly dispatch = injectDispatch(superAdminPageEvents);
 
   protected typeLabel(type?: UserTypeDto) {
     switch (type) {
@@ -22,5 +33,20 @@ export class UserCardComponent {
       default:
         return '';
     }
+  }
+
+  protected onDelete() {
+    this.dialogService.confirm$({
+      zContent: 'Are you sure you want to delete this user?',
+      zOkText: 'Delete',
+      zOkDestructive: true
+    }).subscribe(() => {
+      const id = this.user().id;
+      this.api.deleteUser(id)
+        .pipe(this.notify.notifyHttpRequest('User was deleted'))
+        .subscribe(() => {
+          this.dispatch.opened();
+        });
+    });
   }
 }
