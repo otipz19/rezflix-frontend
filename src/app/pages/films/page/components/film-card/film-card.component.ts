73f829cf41b4b60f7510afa052d98531129d4a22
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-12-02 20:50:25 +0200

    Add star-rating-bar to film card

--- src/app/pages/films/page/components/film-card/film-card.component.ts
+++ src/app/pages/films/page/components/film-card/film-card.component.ts
@@ -1,9 +1,11 @@
 import {ChangeDetectionStrategy, Component, input} from '@angular/core';
 import {FilmDto} from '../../../../../api';
+import {StarRatingBarComponent} from '@shared/components/star-rating-bar/star-rating-bar.component';
 
 @Component({
   selector: 'app-film-card',
   imports: [
+    StarRatingBarComponent
   ],
   templateUrl: './film-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
